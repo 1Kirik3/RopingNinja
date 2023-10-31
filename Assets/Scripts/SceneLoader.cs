01f86e516ed0887eff1bd3d02ce1c99e08fb7930
Daniil Kirkota <95646707+1Kirik3@users.noreply.github.com>
2023-10-31 21:37:39 +0300

    Fix bugs with scrolling store

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,4 +5,7 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(int sceneNumber) 
         => SceneManager.LoadScene(sceneNumber);
+
+    public void ExitGame()
+        => Application.Quit();
 }
