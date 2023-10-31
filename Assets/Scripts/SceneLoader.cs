f0303afea2f634e78d76579f7f679e1217ed5453
Daniil Kirkota <dikirkota@stud.etu.ru>
2023-10-31 17:02:41 +0300

    Implement store interface

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,8 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int sceneNumber) 
+        => SceneManager.LoadScene(sceneNumber);
+}
